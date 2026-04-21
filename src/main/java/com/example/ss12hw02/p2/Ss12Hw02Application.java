b0668d398d6f2dc46163bd8622367183c59f3a8a
TMon283 <lemonboy2k6@gmail.com>
2026-04-21 23:10:17 +0700

    up HW02

---
+++ src/main/java/com/example/ss12hw02/p2/Ss12Hw02Application.java
@@ -0,0 +1,13 @@
+package com.example.ss12hw02.p2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss12Hw02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss12Hw02Application.class, args);
+    }
+
+}
